b228426373a9c4296e98bd6c7f87276e87ae6951
Ale Luna <lunalebizkit@gmail.com>
2021-07-25 09:40:08 -0300

    Repare el nombre sin numero

--- src/lib/passport.js
+++ src/lib/passport.js
@@ -9,7 +9,7 @@ passport.use('local.ingreso', new localStrategy({
     usernameField: 'usuario',
     passwordField: 'contrasenia',
     passReqToCallback: true
-}, async(req, usuario, contrasenia, done) =>{
+}, async(req, usuario, contrasenia, done) =>{    
     const fila= await db.query('SELECT * FROM usuario WHERE usuario =?', [usuario]);
     if (fila.length > 0) {
         const usuario= fila[0];
@@ -21,24 +21,26 @@ passport.use('local.ingreso', new localStrategy({
         }
     }else {
         return done( null, false, req.flash('mal', 'El usuario no existe'));
-    }
-   
+    }   
 }));
-
 passport.use('local.registro', new localStrategy({
         usernameField: 'usuario',
         passwordField: 'contrasenia',
         passReqToCallback: true
 }, async (req, usuario, contrasenia, done) => {
     const {nombre, direccion, cuit, email}= req.body;
-   
+    if (nombre.match(/(\d+)/g)) {
+        return done(null, false, req.flash('mal', 'El Nombre no debe contener Numero!'));
+        }            
+    else {
+        return nombre }
     const comprobar= await db.query('Select * from usuario Where usuario = ?', [usuario]);
     const comprobarEmail = await db.query('SELECT * FROM usuario_email WHERE email =?', [email]);
     if ((comprobar.length)> 0) {
         return done(null, false, req.flash('mal', 'Usuario Existente!'));
     }if ((comprobarEmail.length)>0) {
         return done(null, false, req.flash('mal', 'Email Existente!'));
-    }else {
+    }else {        
         const newUsuario = {
             nombre,
             direccion,
@@ -47,16 +49,13 @@ passport.use('local.registro', new localStrategy({
             contrasenia,
         };
         const mensajeMail =`
-        <h2>Hola! ${nombre}...<br>
-            Gracias por Registrarse a Containers & Logistica !!! </h3>
-        
+        <h3>Hola! ${nombre}...<br>
+            Gracias por Registrarse a Containers & Logistica !!! </h3>        
         <ul>
             <li>Su Usuario es:<b> ${usuario} </b></li>
             <li>Su contraseña es :<b> ${contrasenia} </b></li>
-        </ul>
-        
-    `;
-    
+        </ul> 
+    `;    
     const CLIENT_ID="341263466702-kgegh0q8lvoppkt62du36dnfgvi6hdc8.apps.googleusercontent.com";
     const CLIENT_SECRET="mT3ZTIsG3TMt0p6GCXDjUH2U";
     const REDIRECT_URI="https://developers.google.com/oauthplayground";
@@ -65,10 +64,8 @@ passport.use('local.registro', new localStrategy({
         CLIENT_ID,
         CLIENT_SECRET,
         REDIRECT_URI
-        );
-    
-     oAuth2cliente.setCredentials({refresh_token:REFRESH_TOKEN});
-     
+        );    
+     oAuth2cliente.setCredentials({refresh_token:REFRESH_TOKEN});     
      async function sendMail(){
          try{
             const accessToken= await oAuth2cliente.getAccessToken()
@@ -81,25 +78,18 @@ passport.use('local.registro', new localStrategy({
                      clientSecret:CLIENT_SECRET,
                      refreshToken:REFRESH_TOKEN,
                      accessToken:accessToken
-                 },
-    
+                 },    
              });
-             const mailOptions=
-                
-                {
-                 from:"Contenedores & Logistica <lunalebizkit@gmail.com>",
+             const mailOptions=                
+                {from:"Contenedores & Logistica <lunalebizkit@gmail.com>",
                  to: email,
                  subject:"Confirmacion de Registro",
-                 html: mensajeMail, 
-             };
+                 html: mensajeMail };
              const result = await transporter.sendMail(mailOptions);
-             return result
-    
-    
+             return result    
          }catch(err){
              console.log(err);
-         }
-        
+         }        
      } 
     sendMail()
     //  .then((result)=>res.status(200).send('enviado'))
@@ -109,18 +99,12 @@ passport.use('local.registro', new localStrategy({
         const newEmail = { usuario_id: resultado.insertId, email };
         await db.query('INSERT INTO usuario_email set ?', [newEmail]);
         newUsuario.id= resultado.insertId;
-        return done(null, newUsuario);
-        
-    }
-  
-  
+        return done(null, newUsuario);       
+    }  
 }));
-
-
 passport.serializeUser((user, done)=>{
        done(null, user.id);
    });
-
 passport.deserializeUser(async (id, done)=> {
     const filas= await  db.query('SELECT * FROM usuario Where id = ?', [id]);
     done(null, filas[0]);
